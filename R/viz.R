1388dfa150595005946b51c6c0e701718a661ef5
Gholamali Rahnavard <gholamali.rahnavard@gmail.com>
2018-09-25 15:15:13 -0400

    updated viz and viz_test module

--- R/viz.R
+++ R/viz.R
@@ -27,16 +27,17 @@ nature_theme <- theme_bw() + theme(axis.text.x = element_text(size = 8, vjust = 
 
 # MaAsLin2 heatmap function for overall view of associations 
 maaslin2_heatmap <- function(output_results, title = "", cell_value = "Q.value", data_label = 'Data', metadata_label = 'Metadata',
-                            border_color = "grey93", format =  NA, color = colorRampPalette(c("darkblue","grey90", "darkred"))(50)) {#)
+                             border_color = "grey93", format =  NA, color = colorRampPalette(c("darkblue","grey90", "darkred"))(50)) {#)
   # read MaAsLin output
-
+  
   df <- read.table( output_results,
                     header = TRUE, sep = "\t", fill = TRUE, comment.char = "" , check.names = FALSE)
+  
   if (dim(df)[1] < 1){
     print('There is no association to plot!')
     return (NULL)
   }
-  metadata <- df$Metadata
+  metadata <- df$Variable
   data <- df$Feature
   value <- NA
   # values to use for coloring the heatmap
@@ -60,47 +61,54 @@ maaslin2_heatmap <- function(output_results, title = "", cell_value = "Q.value",
   }
   rownames(a) <- levels(metadata)
   colnames(a) <- levels(data)
-  
-  p <- pheatmap::pheatmap(a, cellwidth = 7, cellheight = 7,   # changed to 3
-                main = title,
-                fontsize = 6,
-                kmeans_k = NA,
-                border=TRUE,
-                show_rownames = T, show_colnames = T,
-                scale="none",
-                #clustering_method = "complete",
-                cluster_rows = FALSE, cluster_cols = TRUE,
-                clustering_distance_rows = "euclidean", 
-                clustering_distance_cols = "euclidean",
-                legend=TRUE,
-                border_color = border_color,
-                color = color,
-                treeheight_row=0,
-                treeheight_col=0,
-                display_numbers = matrix(ifelse(a > 0.0, "+", ifelse(a < 0.0, "-", "")),  nrow(a)))
+  p <- pheatmap::pheatmap(
+    a, cellwidth = 7, cellheight = 7,   # changed to 3
+    main = title,
+    fontsize = 6,
+    kmeans_k = NA,
+    border=TRUE,
+    show_rownames = T, show_colnames = T,
+    scale="none",
+    #clustering_method = "complete",
+    cluster_rows = FALSE, cluster_cols = TRUE,
+    clustering_distance_rows = "euclidean", 
+    clustering_distance_cols = "euclidean",
+    legend=TRUE,
+    border_color = border_color,
+    color = color,
+    treeheight_row=0,
+    treeheight_col=0,
+    display_numbers = matrix(ifelse(a > 0.0, "+", ifelse(a < 0.0, "-", "")),  nrow(a))
+  )
   return(p)
 }
 
 save_heatmap <- function(results_file, heatmap_file, title = "", cell_value = "Q.value", data_label = 'Data', metadata_label = 'Metadata',
-                            border_color = "grey93", format =  NA, color = colorRampPalette(c("blue","grey90", "red"))(50)) {
+                         border_color = "grey93", format =  NA, color = colorRampPalette(c("blue","grey90", "red"))(50)) {
   # generate a heatmap and save it to a pdf
-    heatmap <- maaslin2_heatmap(results_file, title, cell_value, data_label, metadata_label, border_color, color)
-
-    ggplot2::ggsave(filename=heatmap_file, plot=heatmap$gtable, width = 135, height = 100, units = "mm", dpi = 350)
+  heatmap <- maaslin2_heatmap(results_file, title, cell_value, data_label, metadata_label, border_color, color)
+  
+  ggplot2::ggsave(filename=heatmap_file, plot=heatmap$gtable, width = 135, height = 100, units = "mm", dpi = 350)
 }
 
-maaslin2_association_plots <- function(metadata, features,
+maaslin2_association_plots <- function(metadata_path, features_path,
                                        output_results, write_to_file = F, write_to='./')
-  { 
+{ 
   #MaAslin2 scatter plot function and theme
   
   # combine the data and metadata to one datframe using common rows
+  # read MaAsLin output
+  metadata <- read.table( metadata_path, header = TRUE,
+                          row.names = 1, sep = "\t", fill = FALSE, comment.char = "" , check.names = FALSE)
+  features <- read.table( features_path, header = TRUE,
+                          row.names = 1, sep = "\t", fill = FALSE, comment.char = "" , check.names = FALSE)
+  
   common_rows <- intersect(rownames(features), rownames(metadata))
   input_df_all <- cbind(features[common_rows,], metadata[common_rows,])
   
   # read MaAsLin output
   output_df_all <- read.table( output_results, header = TRUE,
-                           row.names = NULL, sep = "\t", fill = FALSE, comment.char = "" , check.names = FALSE)
+                               row.names = NULL, sep = "\t", fill = FALSE, comment.char = "" , check.names = FALSE)
   
   if (dim(output_df_all)[1] < 1){
     print('There is no association to plot!')
@@ -112,7 +120,7 @@ maaslin2_association_plots <- function(metadata, features,
   for (i in 1:dim(output_df_all)[1]){
     #print(i)
     #i <- 1
-    x_label <- as.character(output_df_all[i, 'Metadata'])
+    x_label <- as.character(output_df_all[i, 'Variable'])
     y_label <- as.character(output_df_all[i, 'Feature'])
     input_df <- input_df_all[c(x_label,y_label)]
     colnames(input_df) <- c("x", "y")
@@ -136,22 +144,22 @@ maaslin2_association_plots <- function(metadata, features,
       # fomrat the figure to defualt nature format and add some 
       # statistics (Q.value and Coefficient) to the plot 
       temp_plot <- temp_plot + nature_theme +
-                           annotate(geom="text", x=(max(input_df['x'],na.rm = T) - 
-                                                  .5*(max(input_df['y'], na.rm = T) - 
-                                                  min(input_df['x'], na.rm = T))),
-                                                y=max(input_df['y'], na.rm = T) -0.012, 
-                                   label=paste("Spearman correlation: ", str(output_df[i, 'Coefficient']),"q-value = ",
-                                   str(output_df[i, 'Q.value'])), color="black", size=rel(2), fontface="italic")+
-                           ggplot2::guides(legend.position=NULL)+
-                           theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-                                 panel.background = element_blank(), axis.line = element_line(colour = "black"))
+        annotate(geom="text", x=(max(input_df['x'],na.rm = T) - 
+                                   .5*(max(input_df['y'], na.rm = T) - 
+                                         min(input_df['x'], na.rm = T))),
+                 y=max(input_df['y'], na.rm = T) -0.012, 
+                 label=paste("Spearman correlation: ", str(output_df[i, 'Coefficient']),"q-value = ",
+                             str(output_df[i, 'Q.value'])), color="black", size=rel(2), fontface="italic")+
+        ggplot2::guides(legend.position=NULL)+
+        theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+              panel.background = element_blank(), axis.line = element_line(colour = "black"))
     }
     association_plot[[i]] <- temp_plot 
-      if (write_to_file){
-        ggplot2::ggsave(filename=paste(write_to,'/', i, '.pdf', sep = ''), plot=temp_plot,
-               width = 6.5, height = 5, units = "cm", dpi = 300)
-        #dev.off()
-      }
+    if (write_to_file){
+      ggplot2::ggsave(filename=paste(write_to,'/', i, '.pdf', sep = ''), plot=temp_plot,
+                      width = 6.5, height = 5, units = "cm", dpi = 300)
+      #dev.off()
+    }
   }
   return (association_plot)
 }
