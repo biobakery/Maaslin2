c14f8b1c54b54c87f22bad72bc52bf29b9c25ea5
Gholamali Rahnavard <gholamali.rahnavard@gmail.com>
2018-09-28 09:45:44 -0400

    minor fix

--- R/viz.R
+++ R/viz.R
@@ -10,7 +10,6 @@
 library(ggplot2)
 library(pheatmap)
 #library(ggcorrplot)
-pdf(NULL)
 # MaAsLin2 theme based on Nature journal requirements
 nature_theme <- theme_bw() + theme(axis.text.x = element_text(size = 8, vjust = 1),
                                    axis.text.y = element_text(size = 8, hjust = 1),
