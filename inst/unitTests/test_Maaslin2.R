49bc2280f7166d954f3c64c779e9a01181e624d7
ljmciver <lauren.j.mciver@gmail.com>
2019-05-10 17:57:16 -0600

    Changes needed to pass base check: Move tests to required package location

---
+++ inst/unitTests/test_Maaslin2.R
@@ -0,0 +1,16 @@
+library(testthat)
+library(Maaslin2)
+
+expected_results = read.table('expected_results.tsv',header = TRUE, stringsAsFactors=FALSE)
+setwd("..")
+fit_data <- Maaslin2('Maaslin2/extdata/example1_features.txt','Maaslin2/extdata/example1_metadata.txt','output')
+maaslin_results = read.table('output/significant_results.tsv',header = TRUE, stringsAsFactors=FALSE)
+
+expect_that(expected_results$metadata,equals(maaslin_results$metadata))
+#expect_that(expected_results$feature,equals(maaslin_results$feature))
+#expect_that(expected_results$coef,equals(maaslin_results$coef))
+#expect_that(expected_results$stderr,equals(maaslin_results$stderr))
+#expect_that(expected_results$N,equals(maaslin_results$N))
+#expect_that(expected_results$N.not.0,equals(maaslin_results$N.not.0))
+#expect_that(expected_results$pval.value,equals(maaslin_results$pval.value))
+#expect_that(expected_results$qval.value,equals(maaslin_results$qval.value))
